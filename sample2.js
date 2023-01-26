75fb469532d9df2507448b0e629756afa31bf62c
m.matsumoto <m.matsumoto@lifeperformance.jp>
2023-01-26 11:05:30 +0900

    新規作成

---
+++ sample2.js
@@ -0,0 +1,21 @@
+$(function() {
+    var lists = $('.list li');
+    $(document).on('change', '.serchBox select', function() {
+        lists.show();
+        for (var i = 0; i < $('.serchBox select').length; i++) {
+            // 絞り込みの項目を取得
+            var item = $('.serchBox select').eq(i).attr('name');
+            // 絞り込みの対象を取得
+            var target = $('.serchBox select').eq(i).val();
+ 
+            if(target != '') {
+                for (var j = 0; j < lists.length; j++) {
+                    // 絞り込み対象でない場合は非表示
+                    if(!lists.eq(j).find('.' + item).find('span').hasClass(target)) {
+                        lists.eq(j).hide();
+                    }
+                };
+            }
+        };
+    });
+});
